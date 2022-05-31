0e3b4f284ccdb71fc0c7a7f7b3f4a1fc37cac8f3
Vivek Tomar <86093210+im-vivektomar@users.noreply.github.com>
2022-05-31 23:33:37 +0530

    Update progress.py

--- helper/progress.py
+++ helper/progress.py
@@ -29,7 +29,6 @@ async def progress_for_pyrogram(
             round(percentage, 2))
 
         tmp = progress + "{0} of {1}\n**Speed**: {2}/s\n**ETA**: {3}\n".format(
-            round(percentage, 2),
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
